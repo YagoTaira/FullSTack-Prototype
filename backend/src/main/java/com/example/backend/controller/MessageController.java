82b5c221f59d1ab2880df26ad3b371c2ed119dfc
YagoTaira <yagotaira@gmail.com>
2024-10-06 15:22:16 +0100

    Connected Frontend to Backend

--- backend/src/main/java/com/example/backend/controller/MessageController.java
+++ backend/src/main/java/com/example/backend/controller/MessageController.java
@@ -4,17 +4,19 @@ import com.example.backend.entity.Message;
 import com.example.backend.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/messages")
 public class MessageController {
 
     @Autowired
     private MessageRepository messageRepository;
 
-    @GetMapping("/messages")
+    @GetMapping
     public List<Message> getMessage() {
         return messageRepository.findAll();
     }
